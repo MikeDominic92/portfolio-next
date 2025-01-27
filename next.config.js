aaaa2a73060f5134772af37e3bd1757dfa303dbf
MikeDominic92 <your.email@example.com>
2025-01-27 17:39:15 -0500

    fix: Simplify build configuration for Vercel

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/portfolio-next',
-  assetPrefix: '/portfolio-next/',
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  }
 }
 
 module.exports = nextConfig
